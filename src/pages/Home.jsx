b9e4fabca583e22a2aa14f1866cba28ea0f94f44
Shadow <159432266+SandeshBudhathoki@users.noreply.github.com>
2024-05-15 20:44:10 +0545

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import HomeMain from "../component/home/HomeMain";
+import React from "react";
+import { useEffect } from "react";
+
+function Home() {
+
+  return (
+    <>
+      <HomeMain />
+    </>
+  );
+}
+
+export default Home;
